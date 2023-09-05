dedd574b1b158e8a785fd8d05160622ba9658cf4
Isidor91 <63084653+Isidor91@users.noreply.github.com>
2023-09-05 10:53:02 -0400

    test

---
+++ Untitled-1.py
@@ -0,0 +1,9 @@
+# %%
+print("I know Python and its very easy for everyone")
+
+# %%
+#In Python, a backslash ( \ ) is a continuation character.
+
+print("I know Python and its very easy for everyone")
+
+
